af6f8ae7f80c9351eab103bd3897f6bbcb9e7974
ORTAL1412 <76817204+ORTAL1412@users.noreply.github.com>
2021-08-10 12:20:15 +0300

    asd

--- server/server.js
+++ server/server.js
@@ -1,45 +1,31 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const connectDB = require('./DB/index');
-const express = require('express');
-const cors = require('cors');
+const connectDB = require("./DB/index");
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const path = require('path');
+const newsUserRoute = require("./routes/userRouting");
+const path = require("path");
+const PORT = process.env.PORT || 8000;
+
 app.use(express.json());
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-const newsUserRoute = require('./routes/userRouting')
-connectDB.on('error' ,() => {
-    console.log(("error"));
-})
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,'../client/build')))
-    app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,'../client/build','index.html'))
-})
-}
-
-const PORT =  8000;
-
-
-// app.get('/' , (req , res)=>{
-//     try {
-//         console.log("home page");
-        
-//     } 
-//     catch (error) {
-//         console.log("error pleas try liter");
-//         res.send("error pleas try liter")
-//     }
-    
-// });
-
-app.use('/user' ,newsUserRoute);
-
-
-app.listen(PORT ,(error) => {
-    if(error) return "error"
-      console.log("we're on air");
+connectDB.on("error", () => {
+  console.log("error");
 });
 
+app.listen(PORT, (error) => {
+  if (error) return "error";
+  console.log("we're on air");
+});
+
+app.use("/api/user", newsUserRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
+}
