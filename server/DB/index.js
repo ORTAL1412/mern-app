0d82b4529fe16658bbc66cd609cec5ec7d9a43dd
ORTAL1412 <76817204+ORTAL1412@users.noreply.github.com>
2021-08-10 12:09:31 +0300

    added

--- server/DB/index.js
+++ server/DB/index.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config()
-const URL = process.env.URL;
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
-console.log(URL);
+// console.log(URL);
 
- mongoose.connect(URL, {
+ mongoose.connect(CONNECTION_URL, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
  }) 
- .then(() => { console.log("connecting success")})
- .catch((error)=>{console.error("error connecting! "); })
+ .then(() => console.log("connecting success"))
+ .catch((error)=>console.error("error connecting! "))
 
 
 module.exports = mongoose.connection;
